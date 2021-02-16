9a16d336be806324bb0b66cc6feda49c5a82c27c
phaneendra7400 <phaneendrathatikonda@gmail.com>
2021-02-16 07:54:33 +0000

    the key was provided

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,7 @@
 provider "aws" {
     region      = "ap-south-1"
+  access_key = "AKIAZG3FMK6DVM5IVJMP"
+  secret_key = "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"
 }
 
 resource "aws_instance" "terraform" {
